6e15cce11baee09e9590f9d82baa0f1b84bcc47d
Ameyaa Rajan <ameyaa.rajan@gmail.com>
2026-04-30 10:47:41 -0700

    Update User.java

--- src/main/java/com/tracker/model/User.java
+++ src/main/java/com/tracker/model/User.java
@@ -1,5 +1,19 @@
 package com.tracker.model;
 
 public class User {
+    private String username;
+    private String password;
+    private String persona; 
     
+    public User(String username, String password, String persona){
+        this.username = username;
+        this.password = password;
+        this.persona = persona; 
+    }
+    public String getUser(){ return username; }
+    public void setUser(String username){ this.username = username;}
+    public String getPassword(){  return password; }
+    public void setPassword(String password){ this.password = password;}
+    public String getPersona(){return persona;  }
+    public void setPersona(String persona){ this.persona = persona; }
 }
