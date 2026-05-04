95ae48af495578c0a8a627eaa5ac22a761ef5145
SSanjana17 <ssimhadri350@student.fuhsd.org>
2026-05-04 09:48:15 -0700

    Update Transaction.java

--- src/main/java/com/tracker/model/Transaction.java
+++ src/main/java/com/tracker/model/Transaction.java
@@ -1,5 +1,55 @@
 package com.tracker.model;
+import java.time.LocalDate;
 
-public class Transaction {
-    
+public class Transaction
+{
+private LocalDate date;
+private double amount;
+private String category; 
+private String description;
+
+public Transaction(double amt, String category, String des)
+{
+    date = LocalDate.now();
+    amount = amt;
+    this.category = category;
+    description = des;
 }
+
+public LocalDate getDate()
+{
+    return date;
+}
+
+public double getAmount()
+{
+    return amount;
+}
+
+public String getCategory()
+{
+    return category;
+}
+
+public String getDescription()
+{
+    return description;
+}
+
+public void setAmount(double amount)
+{
+    this.amount = amount;
+}
+
+public void setCategory(String category)
+{
+    this.category = category;
+}
+
+public void setDescription(String description)
+{
+    this.description = description;
+}
+
+}
+
